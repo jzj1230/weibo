be0fc0687ca43a400c5d50c4381be7869e0554d0
jzj <412599577@qq.com>
2019-07-23 16:00:05 +0800

    表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,6 +14,6 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        return view('users/show', compact('user'));
     }
 }
